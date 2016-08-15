07ac3b50b7f008d9c22f09c11e3f31c358611fdf
sakib717 <sakibsabbir7@gmail.com>
2016-08-15 15:08:56 -0400

    added switch case convention for the options to display on output, added intro function for my credits, added function to save record in binary file, display account details, modify record,introductry screen.

--- C++ Final_ProjectBuild-1/C++ Final_ProjectBuild-1.cpp
+++ C++ Final_ProjectBuild-1/C++ Final_ProjectBuild-1.cpp
@@ -85,6 +85,160 @@ char account::rettype() const
 }
 
 
+//   Function Declaration
+//************************
+void write_account();	//function to write record in binary file
+void display_sp(int);	//function to display account details given by user
+void modify_account(int);	//function to modify record of file
+void delete_account(int);	//function to delete record of file
+void display_all();		//function to display all account details
+void deposit_withdraw(int, int); // function to desposit/withdraw amount for given account
+void intro();	//introductory screen function
+
+
+//    	THE MAIN FUNCTION OF PROGRAM
+//***********************************
+
+int main()
+{
+	char ch;
+	int num;
+	intro();
+	do
+	{
+		system("cls");
+		cout << "\n\n\n\tMAIN MENU";
+		cout << "\n\n\t01. NEW ACCOUNT";
+		cout << "\n\n\t02. DEPOSIT AMOUNT";
+		cout << "\n\n\t03. WITHDRAW AMOUNT";
+		cout << "\n\n\t04. BALANCE ENQUIRY";
+		cout << "\n\n\t05. ALL ACCOUNT HOLDER LIST";
+		cout << "\n\n\t06. CLOSE AN ACCOUNT";
+		cout << "\n\n\t07. MODIFY AN ACCOUNT";
+		cout << "\n\n\t08. EXIT";
+		cout << "\n\n\tSelect Your Option (1-8) ";
+		cin >> ch;
+		system("cls");
+		switch (ch)
+		{
+		case '1':
+			write_account();
+			break;
+		case '2':
+			cout << "\n\n\tEnter The account No. : "; cin >> num;
+			deposit_withdraw(num, 1);
+			break;
+		case '3':
+			cout << "\n\n\tEnter The account No. : "; cin >> num;
+			deposit_withdraw(num, 2);
+			break;
+		case '4':
+			cout << "\n\n\tEnter The account No. : "; cin >> num;
+			display_sp(num);
+			break;
+		case '5':
+			display_all();
+			break;
+		case '6':
+			cout << "\n\n\tEnter The account No. : "; cin >> num;
+			delete_account(num);
+			break;
+		case '7':
+			cout << "\n\n\tEnter The account No. : "; cin >> num;
+			modify_account(num);
+			break;
+		case '8':
+			cout << "\n\n\tThank You for using banking management system, have a nice day!";
+			break;
+		default:cout << "\a";
+		}
+		cin.ignore();
+		cin.get();
+	} while (ch != '8');
+	return 0;
+}
+
+//    Function to Write data for file
+//************************************
+
+void write_account()
+{
+	account ac;
+	ofstream outFile;
+	outFile.open("account.dat", ios::binary | ios::app);
+	ac.create_account();
+	outFile.write(reinterpret_cast<char *> (&ac), sizeof(account));
+	outFile.close();
+}
+
+
+//    	Function to Lookup for a Specific Account
+//************************************************
+
+void display_sp(int n)
+{
+	account ac;
+	bool flag = false;
+	ifstream inFile;
+	inFile.open("account.dat", ios::binary);
+	if (!inFile)
+	{
+		cout << "File could not be open !! Press any Key...";
+		return;
+	}
+	cout << "\nBALANCE DETAILS\n";
+
+	while (inFile.read(reinterpret_cast<char *> (&ac), sizeof(account)))
+	{
+		if (ac.retacno() == n)
+		{
+			ac.show_account();
+			flag = true;
+		}
+	}
+	inFile.close();
+	if (flag == false)
+		cout << "\n\nAccount number does not exist";
+}
+
+
+
+//    Function to Modify Record from File
+//****************************************
+
+void modify_account(int n)
+{
+	bool found = false;
+	account ac;
+	fstream File;
+	File.open("account.dat", ios::binary | ios::in | ios::out);
+	if (!File)
+	{
+		cout << "File could not be open !! Press any Key...";
+		return;
+	}
+	while (!File.eof() && found == false)
+	{
+		File.read(reinterpret_cast<char *> (&ac), sizeof(account));
+		if (ac.retacno() == n)
+		{
+			ac.show_account();
+			cout << "\n\nEnter The New Details of account" << endl;
+			ac.modify();
+			int pos = (-1)*static_cast<int>(sizeof(account));
+			File.seekp(pos, ios::cur);
+			File.write(reinterpret_cast<char *> (&ac), sizeof(account));
+			cout << "\n\n\t Record Updated";
+			found = true;
+		}
+	}
+	File.close();
+	if (found == false)
+		cout << "\n\n Record Not Found ";
+}
+
+
+
 //    	INTRODUCTION FUNCTION
 //****************************
 
