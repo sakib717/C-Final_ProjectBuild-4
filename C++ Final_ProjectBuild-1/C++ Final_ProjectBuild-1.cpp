053790af9c5980549f4a5fd1e9575b1302b2c183
sakib717 <sakibsabbir7@gmail.com>
2016-08-15 14:53:54 -0400

    added class account (declaration of values), functionality: create account, show account, deposit, withdraw, outputting as report (tabular format), return type of account, return account number

--- C++ Final_ProjectBuild-1/C++ Final_ProjectBuild-1.cpp
+++ C++ Final_ProjectBuild-1/C++ Final_ProjectBuild-1.cpp
@@ -2,10 +2,98 @@
 //
 
 #include "stdafx.h"
+#include<iostream>
+#include<fstream>
+#include<cctype>
+#include<iomanip>
+using namespace std;
 
 
-int main()
+class account
 {
-    return 0;
+	int acno;
+	char name[50]; //Declare of values for account class 
+	int deposit;
+	char type;
+public:
+	void create_account();	//function to get data from user
+	void show_account() const;	//function to show data on screen
+	//void modify();	//function to add new data
+    void dep(int);	//function to accept amount and add to balance amount
+    void draw(int);	//function to accept amount and subtract from balance amount
+	void report() const;	//function to show data in tabular format
+	int retacno() const;	//function to return account number
+	int retdeposit() const;	//function to return balance amount
+	char rettype() const;	//function to return type of account
+};
+
+
+
+void account::create_account() //for creating an account
+{
+	cout << "\nEnter The account No. :";
+	cin >> acno;
+	cout << "\n\nEnter The Name of The account Holder : ";
+	cin.ignore();
+	cin.getline(name, 50);
+	cout << "\nEnter Type of The account (C/S) : ";
+	cin >> type;
+	type = toupper(type);
+	cout << "\nEnter The Initial amount(>=500 for Saving and >=1000 for current ) : ";
+	cin >> deposit;
+	cout << "\n\n\nCongratulations! Account Created..";
 }
 
+void account::show_account() const                 //for showing the account 
+{
+	cout << "\nAccount No. : " << acno;
+	cout << "\nAccount Holder Name : ";
+	cout << name;
+	cout << "\nType of Account : " << type;
+	cout << "\nBalance amount : " << deposit;
+}
+
+void account::dep(int x) //for deposit and withdraw from account
+{
+	deposit += x; //function to deposit
+}
+
+void account::draw(int x)
+{
+	deposit -= x; //function to withdraw
+}
+
+void account::report() const
+{
+	cout << acno << setw(10) << " " << name << setw(10) << " " << type << setw(6) << deposit << endl;
+}
+
+
+int account::retacno() const
+{
+	return acno;
+}
+
+int account::retdeposit() const
+{
+	return deposit;
+}
+
+char account::rettype() const
+{
+	return type;
+}
+
+
+//    	INTRODUCTION FUNCTION
+//****************************
+
+void intro()
+{
+	cout << "\n\n\n\t  BANKING";
+	cout << "\n\n\tMANAGEMENT";
+	cout << "\n\n\t  SYSTEM";
+	cout << "\n\n\n\nMADE BY : Sakib Patel - 200307608";
+	cout << "\n\nSCHOOL : Georgian College";
+	cin.get();
+}
